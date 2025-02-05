e0726a41f739015db8b855a9475160c7855416fc
SukhpreetSingh0725 <sukhighuman5775@gmail.com>
2025-02-05 19:08:17 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,7 +6,7 @@ int main() {
     scanf("%d\n",&b);
     scanf("%c",&ch);
     if(ch=='+'){
-        printf("%d%c%d=%d",a,b,ch,a+b);
+        printf("%d",a+b);
     } else if(ch=='-'){
         printf("%d",a-b);
     } else if(ch=='*'){
